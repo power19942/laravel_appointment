dc5840556a1fbed31d91e320ac5f048679307e30
omar jakmira <poweromar19942@gmail.com>
2020-05-15 21:34:53 +0300

    laravel db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        factory(User::class)->create([
+            'name' => 'William Jordan',
+            'email' => 'w@w.com',
+            'expert' => 'Doctor',
+            'isExpert' => true,
+            'country' => 'Anabar',
+            'timezone' => 'Pacific/Auckland',
+            'expert_start_time' => new DateTime(now()->format('yy-m-d') . ' 6 pm', new DateTimeZone('Pacific/Auckland')),
+            'expert_end_time' => new DateTime(now()->format('yy-m-d') . ' 5 pm', new DateTimeZone('Pacific/Auckland')),
+        ]);
+        factory(User::class)->create([
+            'name' => 'Quasi Shawa',
+            'email' => 'q@q.com',
+            'expert' => 'Civil engineer',
+            'isExpert' => true,
+            'country' => 'Syria',
+            'timezone' => 'Asia/Damascus',
+            'expert_start_time' => new DateTime(now()->format('yy-m-d') . ' 6 am', new DateTimeZone('Asia/Damascus')),
+            'expert_end_time' => new DateTime(now()->format('yy-m-d') . ' 12 pm', new DateTimeZone('Asia/Damascus')),
+        ]);
+        factory(User::class)->create([
+            'name' => ' Shimaa Badawy',
+            'email' => 's@s.com',
+            'expert' => 'Computer Engineer',
+            'isExpert' => true,
+            'country' => 'Egypt',
+            'timezone' => 'Africa/Cairo',
+            'expert_start_time' => new DateTime(now()->format('yy-m-d') . ' 1 pm', new DateTimeZone('Africa/Cairo')),
+            'expert_end_time' => new DateTime(now()->format('yy-m-d') . ' 2 pm', new DateTimeZone('Africa/Cairo')),
+        ]);
+
     }
 }
