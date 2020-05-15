b0e63fbc84c414fdfdef2aed35993007eed83ed4
omar jakmira <poweromar19942@gmail.com>
2020-05-15 21:36:18 +0300

    update api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login','Api\\AuthController@login');
+Route::post('register','Api\\AuthController@register');
+// Route::get('experts','Api\\ExpertController@index');
+Route::apiResource('experts','Api\\ExpertController');
+Route::apiResource('appointment','Api\AppointmentController');
