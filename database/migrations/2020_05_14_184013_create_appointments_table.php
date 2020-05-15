a365471692456bd3c58093acfd8b416631c21fbc
omar jakmira <poweromar19942@gmail.com>
2020-05-15 21:35:53 +0300

    laravel appointments migration

---
+++ database/migrations/2020_05_14_184013_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('begin');
+            $table->string('end');
+            $table->string('duration');
+            $table->Integer('client_id');
+//           $table->foreign('client_id')->references('id')->on('users')->onDelete('cascade');
+            $table->Integer('expert_id');
+//            $table->foreign('expert_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
