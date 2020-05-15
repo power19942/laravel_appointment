9b13ea489e3444e68f7e656668a14e2496db9df8
omar jakmira <poweromar19942@gmail.com>
2020-05-15 21:30:51 +0300

    configure laravel route for react

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+//    $res =  geoip($r->ip);
+//    return \Illuminate\Http\Request::ip();
+//    dd(  new DateTime('2000-01-01', new DateTimeZone($res['timezone'])));
+//    return $res['timezone'];
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/{any}/{path?}',function (){
+//    return \Carbon\Carbon::today('America/Panama');;
+//    dd(timezone_identifiers_list());
+    return redirect('/');
+});
+//Route::get('/{any}/{path?}','HomeController@index')->name('home');
