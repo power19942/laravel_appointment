db6c91b7d53c91b4859197804182a81f0df63acb
omar jakmira <poweromar19942@gmail.com>
2020-05-15 21:30:06 +0300

    laravel helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,10 +1,27 @@
 <?php
 
-
-namespace App\Helpers;
-
-
-class Helper
+//function convertStringToTimeStamp($time)
+//{
+//    return date("G:i", strtotime($time));
+//
+//}
+function convertTimeStampToTime($timestamp)
 {
+    return date('h:i a', $timestamp);
+}
 
+
+function convertDateToAnotherTimeZone($date, $tz)
+{
+    return (new DateTime($date))->setTimezone(new DateTimeZone($tz));
+}
+
+
+function getTimeFromTimeZone($date,$tz)
+{
+    return (new DateTime($date))->setTimezone(new DateTimeZone($tz))->format('h:i a');
+}
+function getTimeOnly($date)
+{
+    return $date->format('h:i a');
 }
